ba672c9b8a8a00648a6f250f313c981ab6e2464e
Aleksandr K-v <aleks903@rambler.ru>
2020-02-12 13:12:49 +0300

    ahj-hw-6-2

---
+++ src/js/app.js
@@ -0,0 +1,39 @@
+import AddImg from './AddImg.js';
+
+const blockImg = document.getElementsByClassName('block-img')[0];
+const elementError = document.getElementById('error-url');
+
+const buttonSelectFile = document.querySelector('#button-select');
+const elSelectFile = document.querySelector('#drop-file');
+
+const addImg = new AddImg(blockImg, elementError);
+
+function loadFile(files) {
+  for (const item of files) {
+    const urlImg = URL.createObjectURL(item);
+    addImg.createImg('nameImg', urlImg);
+    buttonSelectFile.addEventListener('load', () => {
+      URL.revokeObjectURL(urlImg);
+    });
+  }
+}
+
+elSelectFile.addEventListener('click', () => {
+  buttonSelectFile.value = null;
+  buttonSelectFile.dispatchEvent(new MouseEvent('click'));
+});
+
+elSelectFile.addEventListener('dragover', (event) => {
+  event.preventDefault();
+});
+
+elSelectFile.addEventListener('drop', (event) => {
+  event.preventDefault();
+  const files = Array.from(event.dataTransfer.files);
+  loadFile(files);
+});
+
+buttonSelectFile.addEventListener('input', (event) => {
+  const files = Array.from(event.currentTarget.files);
+  loadFile(files);
+});
